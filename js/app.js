b729f0b9475d32749a32e5d057a249a621f404d8
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2023-04-03 00:18:21 +0300

    testimonials

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Swiper
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
